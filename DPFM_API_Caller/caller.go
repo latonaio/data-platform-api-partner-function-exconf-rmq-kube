0cefd79b9fa4d442a4b30806cc8390a9e0ea67ba
kimura <creative@latona.io>
2023-05-25 16:19:45 +0900

    release

--- DPFM_API_Caller/caller.go
+++ DPFM_API_Caller/caller.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	dpfm_api_input_reader "data-platform-api-partner-function-exconf-rmq-kube/DPFM_API_Input_Reader"
 	dpfm_api_output_formatter "data-platform-api-partner-function-exconf-rmq-kube/DPFM_API_Output_Formatter"
-	"data-platform-api-partner-function-exconf-rmq-kube/database"
-	"sync"
+	"encoding/json"
 
 	"github.com/latonaio/golang-logging-library-for-data-platform/logger"
+	database "github.com/latonaio/golang-mysql-network-connector"
+	rabbitmq "github.com/latonaio/rabbitmq-golang-client-for-data-platform"
+	"golang.org/x/xerrors"
 )
 
 type ExistenceConf struct {
@@ -24,61 +26,56 @@ func NewExistenceConf(ctx context.Context, db *database.Mysql, l *logger.Logger)
 	}
 }
 
-func (e *ExistenceConf) Conf(input *dpfm_api_input_reader.SDC) *dpfm_api_output_formatter.PartnerFunction {
-	partnerFunction := *input.PartnerFunction.PartnerFunction
-	notKeyExistence := make([]string, 0, 1)
-	KeyExistence := make([]string, 0, 1)
+func (e *ExistenceConf) Conf(msg rabbitmq.RabbitmqMessage) interface{} {
+	var ret interface{}
+	ret = map[string]interface{}{
+		"ExistenceConf": false,
+	}
+	input := make(map[string]interface{})
+	err := json.Unmarshal(msg.Raw(), &input)
+	if err != nil {
+		return ret
+	}
 
-	existData := &dpfm_api_output_formatter.PartnerFunction{
-		PartnerFunction: partnerFunction,
+	_, ok := input["PartnerFunction"]
+	if ok {
+		input := &dpfm_api_input_reader.SDC{}
+		err = json.Unmarshal(msg.Raw(), input)
+		ret = e.confPartnerFunction(input)
+		goto endProcess
+	}
+
+	err = xerrors.Errorf("can not get exconf check target")
+endProcess:
+	if err != nil {
+		e.l.Error(err)
+	}
+	return ret
+}
+
+func (e *ExistenceConf) confPartnerFunction(input *dpfm_api_input_reader.SDC) *dpfm_api_output_formatter.PartnerFunction {
+	exconf := dpfm_api_output_formatter.PartnerFunction{
+		ExistenceConf: false,
+	}
+	if input.PartnerFunction.PartnerFunction == nil {
+		return &exconf
+	}
+	exconf = dpfm_api_output_formatter.PartnerFunction{
+		PartnerFunction: *input.PartnerFunction.PartnerFunction,
 		ExistenceConf:   false,
 	}
 
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		if !e.confPartnerFunction(partnerFunction) {
-			notKeyExistence = append(notKeyExistence, partnerFunction)
-			return
-		}
-		KeyExistence = append(KeyExistence, partnerFunction)
-	}()
-
-	wg.Wait()
-
-	if len(KeyExistence) == 0 {
-		return existData
-	}
-	if len(notKeyExistence) > 0 {
-		return existData
-	}
-
-	existData.ExistenceConf = true
-	return existData
-}
-
-func (e *ExistenceConf) confPartnerFunction(val string) bool {
 	rows, err := e.db.Query(
 		`SELECT PartnerFunction 
 		FROM DataPlatformMastersAndTransactionsMysqlKube.data_platform_partner_function_partner_function_data 
-		WHERE PartnerFunction = ?;`, val,
+		WHERE PartnerFunction = ?;`, exconf.PartnerFunction,
 	)
 	if err != nil {
 		e.l.Error(err)
-		return false
+		return &exconf
 	}
+	defer rows.Close()
 
-	for rows.Next() {
-		var partnerFunction string
-		err := rows.Scan(&partnerFunction)
-		if err != nil {
-			e.l.Error(err)
-			continue
-		}
-		if partnerFunction == val {
-			return true
-		}
-	}
-	return false
+	exconf.ExistenceConf = rows.Next()
+	return &exconf
 }
